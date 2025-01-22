3ea90e4354cd5d20990c2ee9086520b2e538be2f
Noololly <95478511+Noololly@users.noreply.github.com>
2025-01-22 20:24:10 +0000

    Initial Commit

---
+++ src/client/java/io/github/noololly/minecralt/MinecraltClient.java
@@ -0,0 +1,10 @@
+package io.github.noololly.minecralt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraltClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
